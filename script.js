796e9d98866f6469be4952e2d9f94fb4d43b4fb7
Maria Meyer <mbean1216@icloud.com>
2022-02-22 16:24:57 -0600

    window prompts

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
-function writePassword() {
+function promptMe() {
+  var passwordLength = prompt("What password length will meet your criteria?");
+  var characterTypes = prompt("What character types do you need? (lowercase, uppercase, numeric, and/or special characters)");
+}
+
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
