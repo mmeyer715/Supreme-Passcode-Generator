de055161fa2c248609bacddf425880eb92d9aca7
Maria Meyer <mbean1216@icloud.com>
2022-02-22 22:27:50 -0600

    restart, added variables

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
-function promptMe() {
-  var passwordLength = prompt("What password length will meet your criteria?");
-  var characterTypes = prompt("What character types do you need? (lowercase, uppercase, numeric, and/or special characters)");
+
+function randomElement(arr){
+  var random = Math.random()
+  var index = Math.floor(random * arr.length)
+  return arr[index]
 }
 
-  function writePassword() {
+var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var special = ['!', '@', '#', '$', '^', '&', '*', '?', '_', '-']
+
+for(var i = 0; i < 128; i++) {
+  if (condition) {
+    
+  } else {
+
+}
+
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
-  passwordText.value = password;
-
 }
 
 // Add event listener to generate button
